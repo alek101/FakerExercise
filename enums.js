4b90bb4e5027e435dfed2c16e24f8bd03da8d570
Aleksandar Petrovic <alekp111@gmail.com>
2021-07-15 10:17:39 +0200

    Create enums.js

---
+++ enums.js
@@ -0,0 +1,3 @@
+catogories=['Kucni Servisi', 'Privatni casovi', 'IT i Dizajn', 'Fizicki poslovi', 'Dostava', 'Trgovina i prodaja',' Ugostiteljstvo',' Online poslovi',' Rad na terenu',' Cuvanje i Nega',' Fitness',' Volontiranje i Ostalo'];
+
+reportReasons=['Lažna Objava', 'Zastareli Sadržaj','Uvredljiv Sadržaj'];
