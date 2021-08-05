5d36cb5fd8c7041199a263307c37001943b32df5
Aleksandar Petrovic <alekp111@gmail.com>
2021-08-05 12:06:31 +0200

    Update postJob.js

--- postJob.js
+++ postJob.js
@@ -68,21 +68,27 @@ async function main(){
   const oneLocation = getOneName(locations);
   const oneCategory = getOneName(categories);
 
+  const longLorem='At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat';
+
   const options = {
     method: 'POST',
     headers: {
       "Content-type":"application/json",
-      "Accept":"application/json",
+      // "Accept":"application/json",
       "Authorization": token
     },
     body: JSON.stringify({
       title: faker.lorem.word(),
       type: 'job',
       description: faker.lorem.sentence(),
+      // description: longLorem,
       category: oneCategory,
       location: oneLocation,
       fromDate: Date.now(),
-      toDate: Date.now()
+      toDate: Date.now(),
+      price: faker.datatype.number({min: 100, max: 1000}),
+      fromTime: faker.lorem.word(),
+      toTime: faker.lorem.word()
     })
   }
 
@@ -90,7 +96,7 @@ async function main(){
 
   const job = await fetchData(web+'/api/v1/jobs',options);
 
-  // await console.log(job);
+  await console.log(job);
   console.log('novi');
 }
 
@@ -100,6 +106,6 @@ function bombardemnt(num=100){
   }
 }
 
-bombardemnt()
+bombardemnt(1)
 
 // console.log(main());
