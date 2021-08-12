4f0a510496904a5d1bcdd295b0aa3811c370f3d2
Aleksandar Petrovic <alekp111@gmail.com>
2021-08-12 13:27:43 +0200

    Update mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,6 +1,20 @@
+const faker = require('faker');
+
 const a=[1,3,5,7];
 
-const b=[2,3,6,9,22];
+const b=[2,3,6,9,22,76];
+
+function makeArray(size, min, max){
+  const array = [];
+  for (let i=0; i<size; i++) {
+    array.push(faker.datatype.number({min, max}));
+  }
+  return array;
+}
+
+const ar = makeArray(20,1,1000);
+
+console.log(ar);
 
 function mergeTwoArrays(ar1, ar2){
   const arr=[];
@@ -17,19 +31,30 @@ function mergeTwoArrays(ar1, ar2){
       arr.push(ar2[j]);
       j++;
     }
+  }
 
-    if(ar1[i] && !ar2[j]){
-      arr.push(ar1[i]);
-      i++;
-    }
+  while(i < len1){
+    arr.push(ar1[i]);
+    i++;
+  }
 
-    if(!ar1[i] && ar2[j]){
-      arr.push(ar2[j]);
-      j++;
-    }
+  while(j < len2){
+    arr.push(ar2[j]);
+    j++;
   }
 
   return arr;
 }
 
-console.log(mergeTwoArrays(a,b));
+function mergeSort(array){
+  
+  const len=array.length;
+  if(len<=1) return array;
+  let lenHalf=Math.floor(len/2);
+  let a=mergeSort(array.slice(0, lenHalf));
+  let b=mergeSort(array.slice(lenHalf,len));
+  return mergeTwoArrays(a, b);
+}
+
+
+console.log(mergeSort(ar))
