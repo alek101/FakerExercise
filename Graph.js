c89bf25280157f0678ca329e8b1c3e4eab123406
Aleksandar Petrovic <alekp111@gmail.com>
2021-08-20 13:21:20 +0200

    Create Graph.js

---
+++ Graph.js
@@ -0,0 +1,26 @@
+class Graph {
+  constructor(){
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex])
+    this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1,vertex2){
+    if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+}
+
+const g = new Graph();
+
+g.addVertex("Tokyo");
+g.addVertex("Dallas");
+g.addVertex("Aspen");
+g.addEdge("Tokyo","Dallas");
+
+console.log(g.adjacencyList);
