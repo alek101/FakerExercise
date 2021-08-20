8831aa0282fca2f6e83046f2397e4320e73b768d
Aleksandar Petrovic <alekp111@gmail.com>
2021-08-20 13:40:08 +0200

    Update Graph.js

--- Graph.js
+++ Graph.js
@@ -14,6 +14,25 @@ class Graph {
       this.adjacencyList[vertex2].push(vertex1);
     }
   }
+
+  removeEdge(vertex1,vertex2){
+    if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+      const i1 = this.adjacencyList[vertex1].indexOf(vertex2);
+      const i2 = this.adjacencyList[vertex2].indexOf(vertex1);
+      this.adjacencyList[vertex1].splice(i1,1);
+      this.adjacencyList[vertex2].splice(i2,1);
+    }
+  }
+
+  removeVertex(vertex){
+    const list = this.adjacencyList[vertex];
+    if(list.length > 0){
+      for(const connection in list){
+        this.removeEdge(vertex,connection);
+      }
+    } 
+    delete this.adjacencyList[vertex];
+  }
 }
 
 const g = new Graph();
@@ -22,5 +41,11 @@ g.addVertex("Tokyo");
 g.addVertex("Dallas");
 g.addVertex("Aspen");
 g.addEdge("Tokyo","Dallas");
+g.addEdge("Tokyo","Aspen");
+g.removeEdge("Tokyo","Aspen");
+g.addVertex("Delete");
+g.addEdge("Tokyo","Delete");
+g.addEdge("Dallas","Delete");
+g.removeVertex("Delete");
 
 console.log(g.adjacencyList);
