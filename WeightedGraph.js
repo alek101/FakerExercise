bc0d188cda5d0ae6f2d124f7fe30e3d992ac694b
Aleksandar Petrovic <alekp111@gmail.com>
2021-08-24 09:26:45 +0200

    Create WeightedGraph.js

---
+++ WeightedGraph.js
@@ -0,0 +1,53 @@
+class SimplePriorityQueue {
+  constructor(){
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    this.values.push({val, priority});
+    this.sort();
+  }
+
+  dequeue() {
+    return this.values.shift();
+  }
+
+  sort(){
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex])
+    this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+
+      this.adjacencyList[vertex1].push({node: vertex2, weight} );
+      this.adjacencyList[vertex2].push({node: vertex1, weight} );
+
+  }
+
+}
+
+const g = new WeightedGraph();
+
+g.addVertex("A")
+
+g.addVertex("B")
+
+g.addVertex("C")
+
+g.addEdge("B", "C", 7)
+
+g.addEdge("A", "C", 5)
+
+g.addEdge("A", "B", 9)
+
+console.log(g.adjacencyList)
