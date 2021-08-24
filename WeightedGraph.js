d558f3838279999c99d5a77303c76170cad21fb3
Aleksandar Petrovic <alekp111@gmail.com>
2021-08-24 13:03:15 +0200

    dijaktra

--- WeightedGraph.js
+++ WeightedGraph.js
@@ -35,20 +35,43 @@ class WeightedGraph {
   dijkstra(start, end){
     const distances={};
     const previous={}
+    const visited = [];
+    const queue = new SimplePriorityQueue();
     Object.keys(this.adjacencyList).forEach(node=>
       {
-        distances[node]=Infinity;
+        if (node == start) {
+          distances[start]=0;
+          queue.enqueue(start, 0);
+        } else {
+          distances[node]=Infinity;
+          queue.enqueue(node, Infinity);
+        }
         previous[node]=null;
       }
     );
-    distances[start]=0;
-    
-    const queue = 
-    
 
+    let current = queue.dequeue();
 
-
-    console.log(distances,previous)
+    while(current.val != end){
+      if(visited.indexOf(current.val)==-1){
+        visited.push(current.val);
+        for (let node of this.adjacencyList[current.val]){
+          let {node: name, weight} = node;
+          if(visited.indexOf(name)==-1){
+            let distance=distances[current.val];
+            distance+=weight;
+            if(distance<distances[name]){
+              distances[name]=distance;
+              previous[name]=current.val;
+              queue.enqueue(name,distance);
+            }
+          }
+        }
+      }
+      current = queue.dequeue();
+    }
+    // console.log('distances', distances, 'previous', previous, 'vistied',visited, 'queue', queue);
+    return distances[end];
   }
 
 }
@@ -56,17 +79,22 @@ class WeightedGraph {
 const g = new WeightedGraph();
 
 g.addVertex("A")
-
 g.addVertex("B")
-
 g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
 
-g.addEdge("B", "C", 7)
 
-g.addEdge("A", "C", 5)
-
-g.addEdge("A", "B", 9)
+g.addEdge("A", "B", 4)
+g.addEdge("A", "C", 2)
+g.addEdge("C", "D", 2)
+g.addEdge("C", "F", 4)
+g.addEdge("F", "E", 1)
+g.addEdge("B", "E", 3)
+g.addEdge("D", "E", 3)
+g.addEdge("D", "F", 1)
 
 // console.log(g.adjacencyList)
 
-g.dijkstra("A","C")
+console.log(g.dijkstra("A","E"))
